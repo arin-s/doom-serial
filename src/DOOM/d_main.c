dc3aba2fe8ba5789d48b36163c40560888eb04bd
arin-s <sarkisianarin@gmail.com>
2025-06-29 16:12:40 +1000

    remove: screen melt/wipe effect

--- src/DOOM/d_main.c
+++ src/DOOM/d_main.c
@@ -193,12 +193,12 @@ void D_Display(void)
     }
 
     // save the current screen if about to wipe
-    if (gamestate != wipegamestate)
+    /*if (gamestate != wipegamestate)
     {
         wipe = true;
         wipe_StartScreen(0, 0, SCREENWIDTH, SCREENHEIGHT);
     }
-    else
+    else*/
         wipe = false;
 
     if (gamestate == GS_LEVEL && gametic)
