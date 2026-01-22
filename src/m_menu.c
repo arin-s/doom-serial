6d0b933cf54c7931009b200557ad93a99280122d
arin-s <sarkisianarin@gmail.com>
2026-01-22 16:45:59 +1100

    change: remove some menu options

--- src/m_menu.c
+++ src/m_menu.c
@@ -241,13 +241,7 @@ enum
 
 menuitem_t MainMenu[]=
 {
-    {1,"M_NGAME",M_NewGame,'n'},
-    {1,"M_OPTION",M_Options,'o'},
-    {1,"M_LOADG",M_LoadGame,'l'},
-    {1,"M_SAVEG",M_SaveGame,'s'},
-    // Another hickup with Special edition.
-    {1,"M_RDTHIS",M_ReadThis,'r'},
-    {1,"M_QUITG",M_QuitDOOM,'q'}
+    {1,"M_NGAME",M_NewGame,'n'}
 };
 
 menu_t  MainDef =
